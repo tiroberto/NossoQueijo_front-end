a15126d5dde42a82603cbe65ef024b371bea7108
Humberto Júnior <Humberto Jnior@DESKTOP-HUMBERT>
2022-04-06 21:25:04 -0300

    .

--- src/Serviços/api.js
+++ src/Serviços/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 const localhost = "https://localhost:44338/api";
+//const webAppAWS = "http://nossoqueijowebapi-dev.sa-east-1.elasticbeanstalk.com/api";
+
 const webAppAWS = "http://nossoqueijowebapi-dev.sa-east-1.elasticbeanstalk.com/api";
 
 const api = axios.create({
