257cf964b70597ff84ff968efb69ea8e1c7b7f1e
Humberto Júnior <Humberto Jnior@DESKTOP-HUMBERT>
2022-04-08 16:05:13 -0300

    .

--- src/Serviços/api.js
+++ src/Serviços/api.js
@@ -2,13 +2,15 @@ import axios from "axios";
 
 const corsEverywhere = "https://cors-everywhere.herokuapp.com/";
 
+
+
 const localhost = "https://localhost:44338/api";
 const webAppAWS = "http://nossoqueijowebapi-dev.sa-east-1.elasticbeanstalk.com/api";
 
 const finalUrl = corsEverywhere+webAppAWS;
 
 const api = axios.create({
-    baseURL: finalUrl,
+    baseURL: webAppAWS
 })
 
 export default api;
